bc0f4024b104f41b718ced2c55ecc33f506fa2bb
Vinh Le <vinhljob@gmail.com>
2019-12-06 07:23:05 -0600

    day04

---
+++ 2019/Day04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day04
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = "138241-674034";
+        }
+    }
+}
