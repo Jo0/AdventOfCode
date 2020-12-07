761350a5e26eb0fd2e346103b151cbaaef7f7a38
Vinh Le <vinhljob@gmail.com>
2020-12-06 21:20:11 -0600

    Finish Day05 and Day06

---
+++ 2020/Day05/Program.fs
@@ -0,0 +1,78 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+
+let getSeatId(boardingPass:string) = 
+    let rec getRow(boardingPass:string, range: int*int) =
+        let midpoint = ((snd range) + (fst range))/2
+
+        match boardingPass with
+        | front when boardingPass.Length > 1 && boardingPass.Substring(0,1) = "F" ->
+            getRow(boardingPass.Substring(1), (fst range, midpoint))
+        | back when boardingPass.Length > 1 && boardingPass.Substring(0,1) = "B"  ->
+            getRow(boardingPass.Substring(1), (midpoint+1, snd range))
+        | finalFront when boardingPass.Length = 1 && boardingPass = "F" ->
+            match finalFront with 
+            | getRange when not (snd range - fst range = 1) ->
+                getRow(finalFront, (fst range, midpoint))
+            | _ -> 
+                fst range
+        | finalBack when boardingPass.Length = 1 && boardingPass = "B" ->
+            match finalBack with 
+            | getRange when not (snd range - fst range = 1) ->
+                getRow(finalBack, (midpoint+1, snd range))
+            | _ -> 
+                snd range
+        | _ -> raise <| new InvalidDataException($"Unknown character in {boardingPass}")
+
+    let rec getCol(boardingPass:string, range: int*int) =
+        let midpoint = ((snd range) + (fst range))/2
+        match boardingPass with
+        | left when boardingPass.Length > 1 && boardingPass.Substring(0,1) = "L"  ->
+            getCol(boardingPass.Substring(1), (fst range, midpoint))
+        | right when boardingPass.Length > 1 && boardingPass.Substring(0,1) = "R" ->
+            getCol(boardingPass.Substring(1), (midpoint+1, snd range))
+        | finalLeft when boardingPass.Length = 1 && boardingPass = "L" ->
+            match finalLeft with 
+            | getRange when not (snd range - fst range = 1) ->
+                getCol(finalLeft, (fst range, midpoint))
+            | _ -> 
+                fst range
+        | finalRight when boardingPass.Length = 1 && boardingPass = "R" ->
+            match finalRight with 
+            | getRange when not (snd range - fst range = 1) ->
+                getCol(finalRight, (midpoint+1, snd range))
+            | _ -> 
+                snd range
+        | _ -> raise <| new InvalidDataException($"Unknown character in {boardingPass}")
+
+    let rowString = boardingPass.Substring(0,7)
+    let columnString = boardingPass.Substring(7,3)
+    let row = getRow(rowString,(0,127))
+    let col = getCol(columnString, (0,7))
+    (row * 8)  + col
+
+[<EntryPoint>]
+let main argv =
+    let input = File.ReadAllLines("input.txt")
+
+    let seatIds = input |> Seq.toList
+                        |> List.map(fun x -> getSeatId(x))
+                        
+    let sortedIds =  seatIds |> List.sort
+    let mySeat = (seatIds |> List.where(fun x -> (not (List.contains(x+1) seatIds)) && (List.contains(x+2) seatIds)) |> List.head) + 1
+                 
+    printf "\n\n\n\n\n\n"
+    printf "%d\n" (List.max seatIds)
+    printf "%d\n" (mySeat) 
+    printf "%d\n" (getSeatId("FBFBBFFRLR"))
+    printf "%d\n" (getSeatId("BFFFBBFRRR"))
+    printf "%d\n" (getSeatId("FFFBBBFRRR"))
+    printf "%d\n" (getSeatId("BBFFBBFRLL"))
+    printf "%d\n" (getSeatId("BFFFFBFLRL"))
+    printf "%d\n" (getSeatId("BFFFFBBLRL"))
+
+    0 // return an integer exit code
+
---
+++ 2020/Day06/Program.fs
@@ -0,0 +1,104 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open System.Collections.Generic
+open System.Text.RegularExpressions
+
+let groupAnswers(input:string list) = 
+    
+    let getGroupAnswers(input:string list) =
+
+        let rec getGroupAnswers(input:string list) =
+            match input with 
+            | [] -> ""
+            | x :: xs -> 
+                match x with 
+                | "" -> "; " + getGroupAnswers(xs)
+                | _ -> x + getGroupAnswers(xs)
+    
+        let groupAnswers = getGroupAnswers(input)
+
+        groupAnswers.Split("; ") |> Seq.toList
+
+    let getGroupQuestions(groupAnswers:string) =
+        let questions = new Dictionary<string, int>()
+        let rec mapAnswersToQuestions(groupAnswers:string, questionsMap: Dictionary<string, int>) =
+            match groupAnswers with 
+            | "" -> questionsMap
+            | answer when Regex.IsMatch(groupAnswers.Substring(0,1), "[a-z]") -> 
+                let question = answer.Substring(0,1)
+                if not (questionsMap.ContainsKey(question)) then questionsMap.Add(question, 1) else questionsMap.[question] <- questionsMap.[question] + 1
+                mapAnswersToQuestions(groupAnswers.Substring(1), questionsMap)
+            | _ -> raise <| Exception $"Not valid question {groupAnswers}"
+
+        mapAnswersToQuestions(groupAnswers, questions)
+
+    let groupAnswers = input |> getGroupAnswers                            
+                             |> List.map(fun x -> getGroupQuestions(x))
+
+    groupAnswers
+
+let groupAnsweredAllQuestions(groupsAnswers: Dictionary<string,int>) =
+    if not (groupsAnswers.Keys.Count = 26) then
+        false
+    else
+        let anAnswer = groupsAnswers.Values |> Seq.toList |> List.head
+        groupsAnswers.Values |> Seq.toList
+                             |> List.forall(fun x -> x = anAnswer)  
+
+let getGroupAnswerByPeople(input: string list) =
+
+    let getGroupAnswers(input:string list) =
+
+        let rec getGroupAnswers(input:string list) =
+            match input with 
+            | [] -> ""
+            | x :: xs -> 
+                match x with 
+                | "" -> "; " + getGroupAnswers(xs)
+                | _ -> x + "," + getGroupAnswers(xs)
+
+        let groupAnswers = getGroupAnswers(input).Split("; ") |> Seq.map(fun x -> x.TrimEnd(','))
+
+        groupAnswers |> Seq.toList
+
+    let getGroupQuestions(groupAnswers:string) =
+        let questions = new Dictionary<string, int>()
+        let rec mapAnswersToQuestions(groupAnswers:string, questionsMap: Dictionary<string, int>) =
+            match groupAnswers with 
+            | "" -> questionsMap
+            | answer when Regex.IsMatch(groupAnswers.Substring(0,1), "[a-z]") -> 
+                let question = answer.Substring(0,1)
+                if not (questionsMap.ContainsKey(question)) then questionsMap.Add(question, 1) else questionsMap.[question] <- questionsMap.[question] + 1
+                mapAnswersToQuestions(groupAnswers.Substring(1), questionsMap)
+            | _ -> raise <| Exception $"Not valid question {groupAnswers}"
+
+        mapAnswersToQuestions(groupAnswers.Replace(",",""), questions)
+
+    let groupAnswers = input |> getGroupAnswers
+                             |> List.map(fun x -> (x.Split(",").Length, getGroupQuestions(x)))
+                             
+    groupAnswers
+                           
+
+[<EntryPoint>]
+let main argv =
+    let input = File.ReadAllLines("input.txt")
+
+    let groupAnswersCount = input |> Seq.toList
+                                  |> groupAnswers
+                                  |> List.sumBy(fun x -> x.Keys.Count)
+                             
+    let groupAnswersCount2 = input |> Seq.toList
+                                   |> getGroupAnswerByPeople
+                                   |> List.sumBy(fun x -> 
+                                                          let numOfPeople = (fst x)
+                                                          let numberOfQuestionsAnsweredByAllPeople = (snd x).Values |> Seq.where(fun v -> v = numOfPeople) |> Seq.length
+                                                          numberOfQuestionsAnsweredByAllPeople
+                                                )
+
+    printf "%d" groupAnswersCount
+    printf "%d" groupAnswersCount2
+
+    0 // return an integer exit code
